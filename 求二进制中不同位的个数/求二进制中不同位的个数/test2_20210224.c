6d72fa4508c804319829b36a99e498c3332b19f4
git-ljp <18967879746@163.com>
2021-04-16 12:01:59 +0800

    二进制中不同位的个数求解方法

---
+++ 求二进制中不同位的个数/求二进制中不同位的个数/test2_20210224.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int Get_diff_bit(int m, int n)
+{
+	int count = 0;
+	int temp = m ^ n;			//利用按位异或原理，相同为0，不同为1；所以只需计算异或之后的二进制数中1的个数就行；
+	while (temp)
+	{
+		temp = temp & (temp - 1);		//每次异或之后都能消除一个1
+		count++;	//异或几次加几次	
+	}
+	return count;
+}
+int main()
+{
+	int m = 0;
+	int n = 0;
+	printf("请输入两个数m和n:");
+	scanf_s("%d%d", &m, &n);
+	int count = Get_diff_bit(m, n);
+	printf("count=%d", count);
+
+	return 0;
+}
