fa43ec18fd3190ecde49376b2a91f3eaf0bfd178
git-ljp <18967879746@163.com>
2021-04-20 17:14:15 +0800

    递归方法求第n个斐波那契数

---
+++ 递归求斐波那契数/递归求斐波那契数/dgfib.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int Fib(int n)
+{
+	//非递归实现
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	int i = 0;
+	for (i = 3; i <= n; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+	}
+	return c;
+	//递归实现
+	/*if (n <= 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return Fib(n - 1) + Fib(n - 2);
+
+	}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+int main()
+{
+	int n = 0;
+	scanf_s("%d", &n);
+
+	int ret = Fib(n);
+	printf("%d", ret);
+	return 0;
+}
