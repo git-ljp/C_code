faacdeb5c25c2a4dc77dc783ec1b99406bc93b86
git-ljp <18967879746@163.com>
2021-07-13 23:14:55 +0800

    描述 KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“*”组成的反斜线形图案。 输入描述： 多组输入，一个整数（2~20），表示输出的行数，也表示组成反斜线的“*”的数量。
    输出描述：
    针对每行输入，输出用“*”组成的反斜线。

---
+++ 反斜线图案/ncbc67.c
@@ -0,0 +1,30 @@
+/*多组输入，一个整数（2~20），表示输出的行数，也表示组成反斜线的“ * ”的数量。
+
+输出描述：
+针对每行输入，输出用“ * ”组成的反斜线。*/
+#include<stdio.h>
+int main()
+{
+    int n = 0;
+    while (~scanf("%d", &n))
+    {
+        int i = 0;
+        //空格加*加空格
+        for (i = 0; i < n; i++)
+        {
+            int j = 0;
+            for (j = 0; j < i; j++)
+            {
+                printf(" ");
+            }
+            printf("*");
+            for (j = 0; j < n - 1 - i; j++)
+            {
+                printf(" ");
+            }
+            printf("\n");
+
+        }
+    }
+    return 0;
+}
