2a5721775381484e99581d304d947c863dc3351d
git-ljp <18967879746@163.com>
2021-07-13 23:18:00 +0800

    模拟memmove实现

---
+++ 模拟实现memmove/memmove.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<assert.h>
+void* my_memmove(void* dest, const void* src, size_t num)
+{
+	void* ret = dest;
+	assert(dest && src);
+	if (dest < src)
+	{
+		while (num--)
+		{
+			*(char*)dest = *(char*)src;
+			dest = (char*)dest + 1;
+			src = (char*)src + 1;
+		}
+	}
+	else
+	{
+		while (num--)
+		{
+			*((char*)dest + num) = *((char*)src + num);
+		}
+
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	my_memmove(arr1, arr1 + 2, 20);
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	return 0;
+}
