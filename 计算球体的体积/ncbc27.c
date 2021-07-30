58a60c2b70664fb414a5f75ac2f85c469ab8e9e3
git-ljp <18967879746@163.com>
2021-07-30 23:55:16 +0800

    给定一个球体的半径，计算其体积。其中球体体积公式为 V = 4/3*πr3，其中 π= 3.1415926。

---
+++ 计算球体的体积/ncbc27.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+# define ¦Ð 3.1415926
+int main()
+{
+    double r = 0;
+    scanf("%lf", &r);
+    double V = (4.0 / 3.0) * ¦Ð * (r * r * r);
+    printf("%.3lf", V);
+    return  0;
+
+}
